d211842e7753abd16e290b2dc1b598882070e394
chadrack-123 <79198387+chadrack-123@users.noreply.github.com>
2025-03-03 17:11:48 +0200

    Cors

--- api/index.py
+++ api/index.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
 import requests
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 def parse_affiliation(raw_str):
     """
